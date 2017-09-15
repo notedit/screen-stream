b2a4ddd8befa9d1f0cceb8627a745be008c660dc
Gong Zhang <zhanggong@me.com>
2017-09-15 17:22:38 +0800

    Add empty swift version recorder

---
+++ screen-stream/screen-stream-swift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  screen-stream-swift
+//
+//  Created by Gong Zhang on 2017/9/15.
+//  Copyright © 2017年 dotEngine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ screen-stream/screen-stream-swift/screen-stream-swift-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CoreVideo/CoreVideo.h>
+#import <DotEngine.h>
+#import <DotStream.h>
+#import <DotView.h>
+#import <DotVideoCapturer.h>
