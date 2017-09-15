52945ad8139843f013156c8daeb820cc56287467
Gong Zhang <zhanggong@me.com>
2017-09-15 16:35:42 +0800

    Add empty swift project

---
+++ screen-stream/screen-stream-watch-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  screen-stream-watch-swift
+//
+//  Created by Gong Zhang on 2017/9/15.
+//  Copyright © 2017年 dotEngine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
