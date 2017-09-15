89a2312abaccfbdf0fc6d6309994bc7381c1efcb
Gong Zhang <zhanggong@me.com>
2017-09-15 17:12:34 +0800

    Add swift version watcher

--- screen-stream/screen-stream-watch-swift/ViewController.swift
+++ screen-stream/screen-stream-watch-swift/ViewController.swift
@@ -8,18 +8,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let APP_KEY = "45"
+let APP_SECRET = "dc5cabddba054ffe894ba79c2910866c"
+let ROOM = "screen_stream"
 
+class ViewController: UIViewController, DotEngineDelegate, DotStreamDelegate {
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var dotEngine: DotEngine!
+    var localStream: DotStream!
+    var remoteStream: DotStream?
+    
+    var isStarted = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dotEngine = DotEngine.sharedInstance(with: self)
+        localStream = DotStream(audio: true, video: false, delegate: self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        startButton.isEnabled = false
+        
+        localStream.setupLocalMedia()
+        
+        let randomNum = arc4random_uniform(10000)
+        let userId = "watch\(randomNum)"
+        dotEngine.generateTestToken(withAppKey: APP_KEY, appsecret: APP_SECRET, room: ROOM, userId: userId) { [weak self] (token, err) in
+            guard err == nil else {
+                print(err!)
+                return
+            }
+            
+            guard let token = token else {
+                print("token is nil")
+                return
+            }
+            
+            self?.dotEngine.joinRoom(withToken: token)
+        }
     }
-
-
+    
+    // MARK: - DotEngine Delegates
+    
+    func dotEngine(_ engine: DotEngine, didJoined peerId: String) {
+        print("didJoined")
+    }
+    
+    func dotEngine(_ engine: DotEngine, didLeave peerId: String) {
+        print("didLeave")
+    }
+    
+    func dotEngine(_ engine: DotEngine, stateChange state: DotStatus) {
+        if state == .connected {
+            dotEngine.add(localStream)
+            startButton.isHidden = true
+        }
+    }
+    
+    func dotEngine(_ engine: DotEngine, didAddLocalStream stream: DotStream) {
+        print("didAddLocalStream")
+    }
+    
+    func dotEngine(_ engine: DotEngine, didRemoveLocalStream stream: DotStream) {
+    }
+    
+    func dotEngine(_ engine: DotEngine, didAddRemoteStream stream: DotStream) {
+        remoteStream = stream
+        stream.view?.frame = CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height)
+        stream.view?.scaleMode = .fit
+        
+        view.addSubview(stream.view!)
+    }
+    
+    func dotEngine(_ engine: DotEngine, didRemoveRemoteStream stream: DotStream) {
+        guard let remoteStream = remoteStream else {
+            return
+        }
+        
+        if remoteStream.streamId == stream.streamId {
+            remoteStream.view?.removeFromSuperview()
+            self.remoteStream = nil
+        }
+    }
+    
+    func dotEngine(_ engine: DotEngine, didOccurError errorCode: DotEngineErrorCode) {
+        print("didOccurError \(errorCode)")
+    }
+    
+    // MARK: - DotStream Delegates
+    
+    func stream(_ stream: DotStream?, didMutedVideo muted: Bool) {
+    }
+    
+    func stream(_ stream: DotStream?, didMutedAudio muted: Bool) {
+    }
+    
+    func stream(_ stream: DotStream?, didGotAudioLevel audioLevel: Int32) {
+    }
+    
 }
 
